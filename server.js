7c0320c3cc2d1f36fa0f6a84d38687da446abab7
becdot <becca.liss@gmail.com>
2017-02-12 14:52:15 -0500

    Basic POST routing

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const upload = require('multer')();
 
 const PORT = process.env.PORT || 3000;
 const NAME = 'Toy Renderer';
 
 const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => {
+const loggingMiddleware = (req, res, next) => {
+  console.log(`received req at ${req.method} ${req.path}`);
+  next();
+};
+
+router.use(loggingMiddleware);
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/', (req, res) => {
   res.send(NAME);
 });
 
-app.get('/status', (req, res) => {
+router.get('/status', (req, res) => {
   res.send('yesok');
 });
+
+router.post('/image', upload.array(), (req, res) => {
+  console.log('req', req.body);
+  res.json(req.body);
+});
+
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`${NAME} listening on port ${PORT}!`);
